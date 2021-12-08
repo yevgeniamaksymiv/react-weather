5ce36589e4533432fb9d243ec3d0f92d51744547
yevgeniamaksymiv <evgeniamaksymiv@gmail.com>
2021-12-08 12:18:29 +0200

    added img changed App.js index.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+const api = {
+  key: "cec0b6382bb0b4c15fefe54bb1557bfa",
+  base: "https://api.openweathermap.org/data/2.5/"
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+          />
+        </div>
+      </main>
     </div>
   );
 }
